abfe1816cdbc222a838dc78e966a03eb309fc2ff
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-06-10 14:54:08 +0200

    output v9

--- src/dotnet/AZ-Fn-Graph/GraphGetUser.cs
+++ src/dotnet/AZ-Fn-Graph/GraphGetUser.cs
@@ -12,27 +12,19 @@ using System.Text;
 
 namespace AZ_Fn_Graph
 {
-    public class GraphGetUser
+    public static class GraphGetUser
     {
-        private readonly ILogger _logger;
-        private readonly Code _code;
-
-        public GraphGetUser(ILogger<GraphGetUser> logger, Code code)
-        {
-            _logger = logger;
-            _code = code;
-        }
         [FunctionName("GraphGetUser")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req, ILogger log, Code code)
         {
-            _logger.LogInformation($"C# HTTP trigger function processed a request");
+            log.LogInformation($"C# HTTP trigger function processed a request");
 
             var parameters = new ParameterObjectBody();
 
-            var graphClient = _code.GetAuthenticatedGraphClient(parameters.appId, parameters.appSecret, parameters.tenantId);
+            var graphClient = code.GetAuthenticatedGraphClient(parameters.appId, parameters.appSecret, parameters.tenantId);
 
-            var user = await _code.GetUser(graphClient, "2fe83868-254d-4787-ad17-00dbd91357b4");
+            var user = await code.GetUser(graphClient, "2fe83868-254d-4787-ad17-00dbd91357b4");
             //var users = await _code.GetUsers(graphClient);
 
             /*StringBuilder responseMessage = new StringBuilder();
