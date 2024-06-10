a24f5e5f6e73600b2cb790d718abd7b7df4a9ca4
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-06-10 13:57:01 +0200

    output v3

--- src/dotnet/AZ-Fn-Graph/GraphGetUser.cs
+++ src/dotnet/AZ-Fn-Graph/GraphGetUser.cs
@@ -7,30 +7,28 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using AZ_Fn_Graph.Helpers;
 
 namespace AZ_Fn_Graph
 {
-    public class GraphGetUser
+    public static class GraphGetUser
     {
-        private readonly ILogger<GraphGetUser> _logger;
-        private readonly Code _code;
-
-        public GraphGetUser(ILogger<GraphGetUser> logger, Code code)
-        {
-            _logger = logger;
-            _code = code;
-        }
         [FunctionName("GraphGetUser")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req,
+            ILogger log)
         {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
 
             string confAppId = Environment.GetEnvironmentVariable("CONF_APP_ID");
             string confAppSecret = Environment.GetEnvironmentVariable("CONF_APP_SECRET");
             string confTenantId = Environment.GetEnvironmentVariable("CONF_TENANT_ID");
             string appInsightsKey = Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY");
 
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
 
             /*string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
