efb2b2ff56bf8c427a65279d49102168133cd187
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-06-11 14:18:07 +0200

    locally working

--- src/dotnet/AZ-Fn-Graph/GraphFunction.cs
+++ src/dotnet/AZ-Fn-Graph/GraphFunction.cs
@@ -2,42 +2,40 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using AZ_Fn_Graph.Helpers;
 using System.Diagnostics;
 using System.Reflection.Metadata;
+using Microsoft.Azure.Functions.Worker;
 
 namespace AZ_Fn_Graph
 {
     public class GraphFunction
     {
         private readonly Code _code;
+        private readonly ILogger _logger;
 
-        public GraphFunction(Code code)
+        public GraphFunction(Code code, ILogger<GraphFunction> logger)
         {
+            _logger = logger;
             _code = code;
         }
 
-        [FunctionName("GraphFunction")]
+        [Function("GraphFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "default")] HttpRequest req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var parameters = new Model();
 
-            /*var graphClient = _code.GetAuthenticatedGraphClient(parameters.tenantId, parameters.appId, parameters.appSecret);
+            var graphClient = _code.GetAuthenticatedGraphClient(parameters.tenantId, parameters.appId, parameters.appSecret);
 
-            var user = await _code.GetUser(graphClient, "92ace2a0-26a3-4432-8254-ea8217fc2d8e");*/
+            var user = await _code.GetUser(graphClient, "92ace2a0-26a3-4432-8254-ea8217fc2d8e");
 
-
-
-            return new OkObjectResult(parameters.appId + "ha");
+            return new OkObjectResult(user.DisplayName);
         }
     }
 }
